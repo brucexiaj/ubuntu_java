703535be2d2a95a6f3cc9dd0de70f97e1b92ba75
brucexiajun <brucexiajun@gmail.com>
2017-03-19 17:18:48 +0800

    Thread_201703191718

--- Thread/src/CyclicBarrier/CyclicBarrierDemo.java
+++ Thread/src/CyclicBarrier/CyclicBarrierDemo.java
@@ -8,7 +8,15 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
+/**
+ * 
+ * @author brucexiajun
+ * CyclicBarrier初始化时规定一个数目，然后计算调用了CyclicBarrier.await()进入等待的线程数。当线程数达到了这个数目时，所有进入等待状态的线程被唤醒并继续。 
+ * CyclicBarrier就象它名字的意思一样，可看成是个障碍， 所有的线程必须到齐后才能一起通过这个障碍。 
+ * CyclicBarrier初始时还可带一个Runnable的参数， 此Runnable任务在CyclicBarrier的数目达到后，所有其它线程被唤醒前被执行
+ * 添加马-》到达数量-》打印栅栏和马的步数-》马醒来-》继续等待
+ * ./Concurrency/New Library Components
+ */
 class Horse implements Runnable
 {
 	private static int counter=0;
@@ -39,14 +47,11 @@ class Horse implements Runnable
 				barrier.await();
 			}
 		}
-		catch(InterruptedException e)
-		{
-			//
-		}
-		catch(BrokenBarrierException e)
+		catch(Exception e)
 		{
 			throw new RuntimeException(e);
 		}
+		
 	}
 	
 	public String toString()
@@ -68,42 +73,38 @@ class Horse implements Runnable
 
 public class CyclicBarrierDemo
 {
-	static final int FINISH_LINE=75;
+	static final int FINISH_LINE=25;//栅栏的数目
 	private List<Horse> horses=new ArrayList<Horse>();
 	private ExecutorService exec=Executors.newCachedThreadPool();
 	private CyclicBarrier barrier;
-	public CyclicBarrierDemo(int nHorses,final int pause)
+	
+	
+	public CyclicBarrierDemo(int nHorses)
 	{
 		barrier=new CyclicBarrier(nHorses,new Runnable(){
-			public void run()
+			public void run()//这个在处于等待数量的马到达数量，马醒来之前执行
 			{
 				StringBuilder sb=new StringBuilder();
 				for(int i=0;i<FINISH_LINE;i++)
 				{
 					sb.append("#");
 				}
-				System.out.println(sb);
+				System.out.println(sb);//打印栅栏
 				for(Horse horse:horses)
 				{
-					System.out.println(horse.tracks());
+					System.out.println(horse.tracks());//打印马当前走过的距离
 				}
+				System.out.println("\n\n");
 				for(Horse horse:horses)
 				{
-					if(horse.getStrides()>=FINISH_LINE)
+					if(horse.getStrides()>=FINISH_LINE)//有没有那匹马已经到达终点
 					{
 						System.out.println(horse+"won!");
 						exec.shutdownNow();
 						return;
 					}
 				}
-				try
-				{
-					TimeUnit.MILLISECONDS.sleep(pause);
-				}
-				catch(InterruptedException e)
-				{
-					System.out.println("barrier-action sleep interrupted");
-				}
+				
 			}
 		});
 		
@@ -111,15 +112,16 @@ public class CyclicBarrierDemo
 		{
 			Horse horse=new Horse(barrier);
 			horses.add(horse);
-			exec.equals(horse);
+			exec.execute(horse);
 		}
 	}
 	
 	public static void main(String args[])
 	{
-		int nHorses=7;
-		int pause=200;
+		int nHorses=5;
 		
-		new CyclicBarrierDemo(nHorses,pause);
+		
+		new CyclicBarrierDemo(nHorses);
+		
 	}
 }
