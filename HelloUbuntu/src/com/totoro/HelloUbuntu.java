e50eb0fab57229c5af9e2ed523023482925ca7fd
brucexiajun <brucexiajun@gmail.com>
2017-03-19 14:50:16 +0800

    HelloUbuntu-201703191435

---
+++ HelloUbuntu/src/com/totoro/HelloUbuntu.java
@@ -0,0 +1,9 @@
+package com.totoro;
+
+public class HelloUbuntu 
+{
+	public static void main(String args[])
+	{
+		System.out.println("This platform was built in 17th.Mar,2017,wish you enjoy it");
+	}
+}
